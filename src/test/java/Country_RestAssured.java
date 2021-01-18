154411f94b991157c4f2d98ff3fc8bd370e174c4
sarvamangala123a <60908560+saruhiremath@users.noreply.github.com>
2021-01-18 17:21:52 -0600

    some methods are modified

--- src/test/java/Country_RestAssured.java
+++ src/test/java/Country_RestAssured.java
@@ -26,27 +26,29 @@ public class Country_RestAssured extends ExtentReportClass {
 
 	}
 
-	@Test(priority=1,enabled=true)
+	@Test(priority = 1, enabled = true)
 	public void test1_getAll_Info() {
 
-		logger = report.startTest("test1_getAllInfo");
+		System.out.println("\ntest1_getAllInfo test is started\n");
+		logger = report.startTest("test1_getAll_Info");
 		logger.log(LogStatus.INFO, "test1_getAllInfo test is started");
 		Response res = RestAssured.get("https://restcountries.eu/rest/v2/all");
 
-		System.out.println("\nResponse ="+ res.asString());
+		System.out.println("\nResponse =" + res.asString());
 		int statusCode = res.getStatusCode();
 		System.out.println("StatusCode= " + statusCode);
 		Assert.assertEquals(statusCode, 200);
 		logger.log(LogStatus.INFO, "statusCode " + statusCode + "is displayed");
 		logger.log(LogStatus.INFO, "Response is displayed");
 		logger.log(LogStatus.PASS, "test1_getAll_Info test is passed");
-
+		System.out.println("test1_getAll_Info test is completed");
 	}
 
-	@Test(priority=2,enabled = true)
+	@Test(priority = 2, enabled = true)
 
 	public void test2_AddCountryCode_ToGetCapital() {
 
+		System.out.println("\ntest2_AddCountryCode_ToGetCapital test is started\n");
 		logger = report.startTest("test2_AddCountryCode_ToGetCapitalName");
 		logger.log(LogStatus.INFO, "test2_AddCountryCode_ToGetCapitalName test is started");
 
@@ -67,22 +69,23 @@ public class Country_RestAssured extends ExtentReportClass {
 				System.out.println("ContentType = " + res.contentType());
 				System.out.println("StatusCode = " + StatusCode);
 				System.out.println("Response body: " + res.asString());
-				System.out.println("Capital = " + res.jsonPath().get("capital"));
+				System.out.println("Capital = " + res.jsonPath().getString("capital"));
 
 				Assert.assertTrue(StatusCode == 200);
 			}
 		}
 		logger.log(LogStatus.INFO, "Capital is displayed and user quit by entering exit ");
 		logger.log(LogStatus.PASS, "test2_get_CapitalName test is passed");
-
+		System.out.println("\ntest2_AddCountryCode_ToGetCapital test is completed");
 	}
 
-	@Test(priority=3,enabled = true)
+	@Test(priority = 3, enabled = true)
 
 	public void test3_AddCountryName_TogetCapital() {
 
 		logger = report.startTest("test3_ get_CapitalName");
 		logger.log(LogStatus.INFO, "test3_get_CapitalName test is started");
+		System.out.println("\ntest3_get_CapitalName test is started\n");
 
 		String name = "notExit";
 
@@ -104,28 +107,34 @@ public class Country_RestAssured extends ExtentReportClass {
 		}
 		logger.log(LogStatus.INFO, "Capital is displayed");
 		logger.log(LogStatus.PASS, "test3_get_CapitalName test is passed");
+		System.out.println("\ntest3_get_CapitalName test is completed");
 		sc.close();
 	}
 
-	@Test(priority=4,enabled = true)
+	@Test(priority = 4, enabled = true)
 	public void test4_Invalid_HTTPMethod() {
 
-		logger = report.startTest("test4_Invalid_HTTPMethod");
-		logger.log(LogStatus.INFO, "test4_Invalid_HTTPMethod test is started");
-		Response res = RestAssured.post("https://restcountries.eu/rest/v2/alpha?codes=IN");
+		logger = report.startTest("\ntest4_Invalid_HTTPMethod\n");
+		logger.log(LogStatus.INFO, "test4_Invalid_HTTPMethod test is started\n");
+		System.out.println("test4_Invalid_HTTPMethod test is started");
+		
+		Response res = RestAssured.put("https://restcountries.eu/rest/v2/alpha?codes=IN");
 
 		int statusCode = res.getStatusCode();
 
 		System.out.println("StatusCode= " + statusCode);
-		Assert.assertEquals(statusCode, 405);
-		logger.log(LogStatus.INFO, "statusCode "+statusCode+" is displayed");
-		logger.log(LogStatus.PASS, "test4_Invalid_HTTPMethod test is passed");
-
+		
+		 Assert.assertEquals(statusCode, 405);
+		
+		logger.log(LogStatus.INFO, "statusCode " + statusCode + " is displayed");
+		logger.log(LogStatus.PASS, "test4_Invalid_HTTPMethod test is completed");
+        System.out.println("test4_Invalid_HTTPMethod test is completed");
 	}
 
-	@Test(priority=5,enabled = false)
+	@Test(priority = 5, enabled = true)
 	public void test5_Invalid_URL() {
-
+        
+		System.out.println("\ntest5_Invalid_URL test is started\n");
 		logger = report.startTest("test5_Invalid_URL");
 		logger.log(LogStatus.INFO, "test5_Invalid_URL test is started");
 		Response res = RestAssured.get("https://restcountries.eu/rest/v2//alpha?codes=IN");
@@ -133,9 +142,9 @@ public class Country_RestAssured extends ExtentReportClass {
 		int statusCode = res.getStatusCode();
 		System.out.println("StatusCode= " + statusCode);
 		Assert.assertEquals(statusCode, 404);
-		logger.log(LogStatus.INFO, "statusCode "+statusCode +" is displayed");
-		logger.log(LogStatus.PASS, "test5_Invalid_URL test is passed");
-
+		logger.log(LogStatus.INFO, "statusCode " + statusCode + " is displayed");
+		logger.log(LogStatus.PASS, "test5_Invalid_URL test is completed");
+		System.out.println("\ntest5_Invalid_URL test is completed");
 	}
 
 	@AfterClass
